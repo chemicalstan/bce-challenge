4355faeba861df4dba9ee04f9e70bf538dc250d9
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2022-02-22 15:18:19 +0100

    Inject port.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import config from './config';
 
-const {
-  appKey: { port },
-} = config;
+const port = process.env.PORT;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
