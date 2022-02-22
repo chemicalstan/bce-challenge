e9bdee34d6abbca9c233b738c6ca728db193dd64
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2022-02-22 15:12:45 +0100

    Implement end to end test.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,23 +2,103 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { User } from 'src/user/user.model';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+
+  const validUser: User = {
+    account_name: 'Okonkwo Chukwebuka Stanley',
+    account_number: '0229960656',
+    bank_code: '058',
+  };
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterEach(async () => {
+    jest.setTimeout(8000);
+    await app.close();
+  });
+
+  const gql = '/graphql';
+
+  describe('verify user', () => {
+    it('should verify user', async () => {
+      return request(app.getHttpServer())
+        .post(gql)
+        .send({
+          query: `mutation {verifyUser(payload: {user_bank_code: "058",user_account_name: "OKONKWO chukwebuka STANLEY",user_account_number: "0229960656"}){is_verified account_name account_number bank_code}}`,
+        })
+        .expect(200)
+        .expect(({ body }) => {
+          expect(body.data.verifyUser).toEqual({
+            is_verified: true,
+            account_name: 'OKONKWO chukwebuka STANLEY'.toLowerCase(),
+            account_number: '0229960656',
+            bank_code: '058',
+          });
+        });
+    });
+
+    it('return unverify user', async () => {
+      return request(app.getHttpServer())
+        .post(gql)
+        .send({
+          query: `mutation {verifyUser(payload: {user_bank_code: "058",user_account_name: "chukwebuka STANLEY",user_account_number: "0229960656"}){is_verified account_name account_number bank_code}}`,
+        })
+        .expect(200)
+        .expect(({ body }) => {
+          expect(body.data.verifyUser).toEqual({
+            is_verified: false,
+            account_name: 'chukwebuka STANLEY'.toLowerCase(),
+            account_number: '0229960656',
+            bank_code: '058',
+          });
+        });
+    });
+  });
+
+  describe('query user', () => {
+    it("should return user's input", async () => {
+      let verified_user;
+      return request(app.getHttpServer())
+        .post(gql)
+        .send({
+          query: `mutation {verifyUser(payload: {user_bank_code: "058",user_account_name: "OKONKO chukwebuka STANLEY",user_account_number: "0229960656"}){is_verified account_name account_number bank_code}}`,
+        })
+        .expect(200)
+        .expect(({ body }) => {
+          verified_user = body.data.verifyUser;
+        })
+        .then(() => {
+          request(app.getHttpServer())
+            .post(gql)
+            .send({
+              query: `query{queryUser(payload:{account_number:"0229960656", bank_code:"058"})}`,
+            })
+            .expect(200)
+            .expect(({ body }) => {
+              expect(body.data.queryUser).toEqual(verified_user.account_name);
+            });
+        });
+    });
+
+    it('return paystacks data', async () => {
+      return request(app.getHttpServer())
+        .post(gql)
+        .send({
+          query: `query{queryUser(payload:{account_number:"0229960656", bank_code:"058"})}`,
+        })
+        .expect(200)
+        .expect(({ body }) => {
+          expect(body.data.queryUser).toEqual(validUser.account_name);
+        });
+    });
   });
 });
